0c5e61043e8c2437e0ab5b10ba47be5b23f9897c
inadequacy <cole.brookslyon@gmail.com>
2026-02-17 21:18:39 +0100

    ex1 done; error handling

--- python_02/ex1/ft_different_errors.py
+++ python_02/ex1/ft_different_errors.py
@@ -1,14 +1,48 @@
 #!/usr/bin/env python3
 
-
+def garden_operations() -> None:
     try:
-        pass
-    except:
-        pass
+        print("Testing ValueError...")
+        int("abc")
+    except ValueError:
+        print("Caught ValueError: invalid literal for int()\n")
+    try:
+        print("Testing ZeroDivisionError...")
+        (5 / 0)
+    except ZeroDivisionError:
+        print("Caught ZeroDivisionError: division by zero\n")
+    try:
+        print("Testing FileNotFoundError...")
+        name = "missing.txt"
+        open(name, "r")
+    except FileNotFoundError:
+        print("Caught FileNotFoundError: No such file '{name}'\n")
+    try:
+        print("Testing KeyError...")
+        nursery = {}
+        nursery["cactus"]
+    except KeyError:
+        print("Caught KeyError: 'missing\\_plant'\n")
+    try:
+        print("Testing multiple errors together...")
+        nursery = {}
+        open(nursery["cactus"], "r")
+        int(10 / 0)
+    except (ValueError, KeyError,
+            ZeroDivisionError,
+            FileNotFoundError):
+        print("Caught an error, but program continues!\n")
     else:
         pass
     finally:
         pass
 
+
+def test_error_types() -> None:
+    print("===\tGarden Error Types Demo\t===\n")
+    garden_operations()
+    print("All error types tested succesfully!")
+
+
 if __name__ == "__main__":
-    pass
+    test_error_types()
