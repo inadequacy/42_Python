1598b28633c237591009b43960715ed55eaed97f
inadequacy <cole.brookslyon@gmail.com>
2026-02-26 11:26:51 +0100

    Update ft_coordinate_system.py
    wip, gotta polish, seems to work as intended

--- python_03/ex2/ft_coordinate_system.py
+++ python_03/ex2/ft_coordinate_system.py
@@ -1,10 +1,45 @@
 #!/usr/bin/env python3
 import sys
+import math
 
 
-def score_analysis() -> None:
-    pass
+def coordinate_system() -> None:
+    argc = len(sys.argv)
+    if argc != 4 and argc != 2:
+        print("No coordinates provided. Usage: "
+              "python3 ft_coordinate_system.py \"x,y,z\" or <x> <y> <z>...")
+        return
+
+    nbrs = [0] * 3
+    if argc == 2:
+        print(f"Parsing coordinates: \"{sys.argv[1]}\"")
+        nbrs = sys.argv[1].split(",", 2)
+    try:
+        if argc == 4:
+            for i in range(1, 3):
+                nbrs[(i - 1)] = int(sys.argv[i])
+    except ValueError:
+        print("Error parsing coordinates: "
+              f"invalid literal for int() with base 10: '{sys.argv[i]}'")
+        print("Error details - Type: ValueError, Args: "
+              f"(\"invalid literal for int() with base 10: '{sys.argv[i]}'\")")
+        return
+    
+    coords = f"{nbrs[0]}, {nbrs[1]}, {nbrs[2]}"
+    x1: int = 0
+    y1: int = 0
+    z1: int = 0
+    x2 = int(nbrs[0])
+    y2 = int(nbrs[1])
+    z2 = int(nbrs[2])
+    dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+    if argc == 4:
+        print(f"Position created: ({coords})")
+    else: 
+        print(f"Parsed position: ({coords})")
+    print(f"Distance between (0, 0, 0) and ({coords}): {dist}")
+    
 
 
 if __name__ == "__main__":
-    score_analysis()
+    coordinate_system()
