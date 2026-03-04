ac25b91bcb4aaa20ccf18526f41f5c8100fee11f
inadequacy <cole.brookslyon@gmail.com>
2026-03-04 22:27:31 +0100

    sets wip

--- python_03/ex3/ft_achievement_tracker.py
+++ python_03/ex3/ft_achievement_tracker.py
@@ -2,9 +2,17 @@
 import sys
 
 
-def run_name() -> None:
-  pass
-
+def achievement_tracker() -> None:
+    alice = {'first_kill', 'level_10', 'treasure_hunter', 'speed_demon'}
+    bob =  {'first_kill', 'level_10', 'boss_slayer', 'collector'}
+    charlie = {'level_10', 'treasure_hunter', 'boss_slayer', 'speed_demon', 'perfectionist'}
+    players = [alice, bob, charlie]
+    player_names = ["alice", "bob", "charlie"]
+    print("===\tAchievement Tracker System\t===\n")
+    i = 0
+    for n in players:
+        print(f"Player {player_names[i]} achievements {n}")
+        i += 1
 
 if __name__ == "__main__":
-    run_name()
+    achievement_tracker()
