35df2a30ea4bd331fb7b72fe6975598bc2a87eb8
inadequacy <cole.brookslyon@gmail.com>
2026-03-09 16:24:41 +0100

    Update ft_garden_summary.py

--- python_00/ex6/ft_garden_summary.py
+++ python_00/ex6/ft_garden_summary.py
@@ -1,4 +1,4 @@
-def ft_garden_summary():
+def ft_garden_summary() -> None:
     garden = str(input("Enter garden name: "))
     plants = int(input("Enter number of plants: "))
     print(f"Garden: {garden}\nPlants: {plants}\nStatus: Growing well!")
