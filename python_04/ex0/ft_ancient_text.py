9c86ca6029d8aa36c801f6cbe66edbab8b10fc80
inadequacy <cole.brookslyon@gmail.com>
2026-03-20 18:04:57 +0100

    Update ft_ancient_text.py

--- python_04/ex0/ft_ancient_text.py
+++ python_04/ex0/ft_ancient_text.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
-
 def function() -> None:
-    pass
+    to_open = "./ancient_fragment.txt"
+    try:
+        file = open(to_open, "r")
+    except (UnboundLocalError, FileNotFoundError, TypeError):
+        print("ERROR: Storage vault not found. Run generator first")
+        return
+    print(f"Storage accessed: {to_open}")
+    content = file.read()
+    print(f"Recovered Data: \n{content}")
+    file.close()
+    print("Storage unit closed.")
 
 
 if __name__ == "__main__":
