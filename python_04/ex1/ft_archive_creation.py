98aec03f9df7b2af7446d68982659f8ed46eb8ba
inadequacy <cole.brookslyon@gmail.com>
2026-03-23 18:45:22 +0100

    Update ft_archive_creation.py

--- python_04/ex1/ft_archive_creation.py
+++ python_04/ex1/ft_archive_creation.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
 
+
+def function() -> None:
+    file_name = "new_discovery.txt"
+    file = open(file_name, "w")
+    print(f"New file created: {file_name}")
+    print("Adding following lines:")
+    entries = ("[ENTRY 001] New quantum algorithm discovered\n"
+               "[ENTRY 002] Efficiency increased by 347%\n"
+               "[ENTRY 003] Archived by Data Archivist trainee")
+    file.write(entries)
+    print(entries)
+    print("Lines added, saved for future use.")
+    file.close()
+
+
+if __name__ == "__main__":
+    function()
