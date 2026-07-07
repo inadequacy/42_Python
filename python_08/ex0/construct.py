b333df45e1da6906ea1acc3238223b466c01ec5a
inadequacy <cole.brookslyon@gmail.com>
2026-07-07 16:18:49 +0100

    Update construct.py

--- python_08/ex0/construct.py
+++ python_08/ex0/construct.py
@@ -1,12 +1,15 @@
-import typing
 import sys
-
+import site
 
 def CheckInsideEnv() -> None:
     if sys.prefix == sys.base_prefix:
-        print("Not in a virtual environment; use 'python3 -m venv matrix_env\n'" \
-              "followed by 'source matriv_env/bin/activate'")
+        print(f"Current Python: {sys.version}\n" \
+              "Not in a virtual environment; use 'python -m venv matrix_env\n'" \
+              "followed by 'source matriv_env/bin/activate' # On Unix or\n" \
+              "followed by 'matriv_env\Scripts\activate' # On Windows")
         return
     else:
-        print(f"You are in a Virtual Environment. Source: {sys.prefix}")
+        print(f"You are in a Virtual Environment. Source: {sys.prefix}\n" \
+              f"Current Python: {sys.version}" \
+              f"{site.getsitepackages()}")
         return
